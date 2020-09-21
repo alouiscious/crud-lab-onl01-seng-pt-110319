b5344a61783741d7b8ea673a8267e438bd828f41
Alouiscious Media <info@reneesoul.com>
2020-09-21 00:24:11 -0700

    Update RestaurantsContainer.js

--- src/containers/RestaurantsContainer.js
+++ src/containers/RestaurantsContainer.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import RestaurantInput from '../components/restaurants/RestaurantInput';
 import Restaurants from '../components/restaurants/Restaurants';
-
+import { Provider } from "redux";
 class RestaurantsContainer extends Component {
 
   render() {
     return (
       <div>
+        
         <RestaurantInput />
         <Restaurants />
       </div>
